d5f69bd2ed2de1f4d1ad6bb05984244c2e332930
emreozcan3320 <emreozcan3320@gmail.com>
2018-11-18 23:25:02 +0300

    landing page text development started

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -5,7 +5,16 @@ class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context ){
     return new Material(
-      color : Colors.greenAccent
+      color : Colors.greenAccent,
+      child: new InkWell(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text("Lets Quizzz",style: new TextStyle(color: Colors.white, fontSize: 50.0, fontWeight: FontWeight.bold),),
+            new Text("Tab to start !",style: new TextStyle(color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold),)
+          ],
+        ),
+      ),
     );
   }
 }
